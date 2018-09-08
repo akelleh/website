2f7dc4cc938c0023e94df27de6047b4448618cd1
adam <akelleh@gmail.com>
2018-09-08 17:08:17 -0400

    added event abstraction

--- website/util.py
+++ website/util.py
@@ -1,4 +1,10 @@
 import urllib3
+import time
+import uuid
+
+
+class CookieNotSet(Exception):
+    pass
 
 
 class LoggerClient(object):
@@ -8,3 +14,26 @@ class LoggerClient(object):
 
     def log(self, event):
         self.http.request('GET', self.url, fields=event)
+
+
+class Event(object):
+    def __init__(self, request_handler, page_id=None):
+        self.request_handler = request_handler
+        self.event_features = {}
+        self.get_or_set_cookie()
+        self.event_features['event_id'] = str(uuid.uuid4())
+        self.event_features['ts'] = time.time()
+        self.event_features['page_id'] = page_id
+        print(self.event_features)
+
+    def log(self):
+        self.request_handler.application.logger_client.log(self.event_features)
+
+    def get_or_set_cookie(self):
+        try:
+            self.event_features['user_id'] = self.request_handler.get_cookie("user_id")
+            if not self.event_features['user_id']:
+                raise CookieNotSet
+        except CookieNotSet:
+            self.event_features['user_id'] = str(uuid.uuid4())
+            self.request_handler.set_cookie("user_id", self.event_features['user_id'])
