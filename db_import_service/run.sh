7826aa5edb5761f1d0f1b5b794f140b60f6c7ecf
adam <akelleh@gmail.com>
2018-12-01 22:10:23 -0500

    updated run

--- db_import_service/run.sh
+++ db_import_service/run.sh
@@ -1,7 +1,11 @@
-docker build -t logger .
-docker run -p 8889:8889 \
+docker build -t importer .
+docker run -p 8890:8890 \
 -e AWS_ACCESS_KEY_ID=`echo $AWS_ACCESS_KEY_ID` \
 -e AWS_SECRET_ACCESS_KEY=`echo $AWS_SECRET_ACCESS_KEY` \
+-e MYSQL_USERNAME=`echo $MYSQL_USERNAME` \
+-e MYSQL_PASSWORD=`echo $MYSQL_PASSWORD` \
+-e MYSQL_SERVER_ADDRESS=`echo $MYSQL_SERVER_ADDRESS` \
+-e MYSQL_DATABASE=`echo $MYSQL_DATABASE` \
 --restart unless-stopped \
---name logger \
--it logger
+--name importer \
+-it importer
