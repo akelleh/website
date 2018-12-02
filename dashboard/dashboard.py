42d5b30f223fe38a82cbf0b86e47eb4ddf4ef3be
adam <akelleh@gmail.com>
2018-12-02 14:45:40 -0500

    added tornado server

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,27 +1,11 @@
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from util import generate_table, LightClient
-import plotly.graph_objs as go
-from plotting import traffic_plot
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from flask_dashboard import server
 
-import numpy as np
-import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+port = 8051
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-N = 100
-df = pd.DataFrame({'$x_1$': np.random.normal(size=N), '$x_2$': np.random.normal(size=N)})
-
-app.layout = html.Div(children=[
-    html.H1(children='adamkelleher.com'),
-
-    html.Div(children=[html.H2(children="Traffic Over Time"),]),
-    traffic_plot()
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+http_server = HTTPServer(WSGIContainer(server))
+http_server.listen(port)
+IOLoop.instance().start()
---
+++ dashboard/flask_dashboard.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from util import generate_table, LightClient
+import plotly.graph_objs as go
+from plotting import traffic_plot
+import flask
+
+import numpy as np
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__,
+                external_stylesheets=external_stylesheets,
+                server=server)
+
+app.layout = html.Div(children=[
+    html.H1(children='adamkelleher.com'),
+
+    html.Div(children=[html.H2(children="Traffic Over Time"),]),
+    traffic_plot()
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
