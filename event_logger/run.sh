ad02732e8c5d6e1f9ce5ecfcf1b61148a7b8e54d
adam <akelleh@gmail.com>
2018-12-02 21:25:11 -0500

    fixed logger run script :

--- event_logger/run.sh
+++ event_logger/run.sh
@@ -1,8 +1,8 @@
-docker pull akelleh/logger
+docker pull akelleh/event_logger
 docker run -p 8889:8889 \
 -e AWS_ACCESS_KEY_ID=`echo $AWS_ACCESS_KEY_ID` \
 -e AWS_SECRET_ACCESS_KEY=`echo $AWS_SECRET_ACCESS_KEY` \
 --restart unless-stopped \
 --network website_network \
 --name logger \
--it akelleh/logger
+-it akelleh/event_logger
