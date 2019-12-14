556d66d34f8ad622036788ace3995b6e0fdcab1f
Adam Kelleher <akelleh@gmail.com>
2019-12-14 16:03:43 -0500

    added aws config

--- security_camera/run.sh
+++ security_camera/run.sh
@@ -4,4 +4,6 @@ docker run \
 --restart unless-stopped \
 --name security_camera \
 --device /dev/video0:/dev/video0 \
+-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 -it security_camera
