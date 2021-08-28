14fbad35d881c41511d0c7754907dc1a73d43867
Adam Kelleher <akelleh@gmail.com>
2021-08-28 08:58:27 -0400

    added main and boot files

---
+++ iot_sensor/boot.py
@@ -0,0 +1,22 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+import webrepl
+webrepl.start()
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('NETGEAR07', 'windyocean044')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+do_connect()
+gc.collect()
